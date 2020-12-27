dd51f760ce3f93977647de6fb0ccad3d6cb9bfb1
hakob <hakobhn@yahoo.com>
2020-12-27 08:58:52 +0400

    Dockerizateion corrections

--- src/main/java/am/neovision/admin/toolkit/App.java
+++ src/main/java/am/neovision/admin/toolkit/App.java
@@ -1,6 +1,7 @@
 package am.neovision.admin.toolkit;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
