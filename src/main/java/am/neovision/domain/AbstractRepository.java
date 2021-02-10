0c3f40064cef77e9807f6c5d1d9d57b3f8b80065
Hakob Hakobyan <hakobhn@yahoo.com>
2021-02-10 21:09:21 +0400

    docker-compose.yml corrections

--- src/main/java/am/neovision/domain/AbstractRepository.java
+++ src/main/java/am/neovision/domain/AbstractRepository.java
@@ -3,9 +3,11 @@ package am.neovision.domain;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 
+@NoRepositoryBean
 public interface AbstractRepository<E, S extends Serializable> extends JpaRepository<E, S> {
 
     Page<E> findBySearch(String search, Pageable pageable);
